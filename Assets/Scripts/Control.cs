45908bebd01d85c6d145543a99c8535d5eea044c
Serik Ólafur Ásgeirsson <43746680+kartoflu-supa@users.noreply.github.com>
2020-02-27 02:44:30 +0000

    comment

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -6,12 +6,11 @@ public class Control : MonoBehaviour
     public float sped = 1000;
     bool jump = false;
     Vector3 spawn;
-    // Start is called before the first frame update
     void Start()
     {
-        spawn = transform.position;
+        spawn = transform.position; //creates the spawn point
     }
-
+//checks if player is grounded
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "grund")
@@ -21,21 +20,17 @@ public class Control : MonoBehaviour
     }
     private void Update()
     {
+    //checks if player has fallen of the map and resets them
         if (transform.position.y < 0)
         {
             transform.SetPositionAndRotation(spawn,transform.rotation);
+            gravity.velocity = Vector3.zero;
         }
     }
-    // Update is called once per frame
+    
     void FixedUpdate()
     {
-        if (jump)
-        {
-            if (transform.position.y == 1)
-            {
-                jump = false;
-            }
-        }
+    //controls for the player to use
         if (Input.GetKey("d"))
         {
             gravity.AddForce(sped * Time.deltaTime, 0, 0);
@@ -52,6 +47,7 @@ public class Control : MonoBehaviour
         {
             gravity.AddForce(0, 0, -sped * Time.deltaTime);
         }
+    //allow the player to jump only when they aren't jumping
         if (Input.GetKeyDown(KeyCode.Space) && jump == false)
         {
             gravity.AddForce(0, 20000 * Time.deltaTime, 0);
