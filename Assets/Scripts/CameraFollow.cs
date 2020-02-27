c319cfc01472b233fbf630af7cfb3d027c608ad4
Serik Ólafur Ásgeirsson <43746680+kartoflu-supa@users.noreply.github.com>
2020-02-27 02:46:55 +0000

    comment

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,16 +6,12 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform playerPos;
     public Vector3 offset;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void FixedUpdate()
     {
+    //places the camera above the player with an offset
         transform.position = playerPos.position + offset;
+    //increases the height or lowers it when the user wants
         if (Input.GetKey("q"))
         {
             offset.y += 2;
